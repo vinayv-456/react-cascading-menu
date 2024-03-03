7ad525f0a5da3b78b7c81fe584ca49a68a5684cf
vinayv-456 <vinayvallabineni456@gmail.com>
2024-03-03 14:39:07 -0500

    feat: add example for checking the changes from direct src or from build

---
+++ example/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Index from "../../src/index"; // dynamic changes
+// import Index from "../../dist/bundle";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Your React App</h1>
+      <Index />
+    </div>
+  );
+};
+
+export default App;
