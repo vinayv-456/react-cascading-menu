5f8590d58dc87c2dcf6511fba3619a26bc631bdd
vinayv-456 <vinayvallabineni456@gmail.com>
2024-03-03 20:34:40 -0500

    WIP

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface Props {}
+
+const Dropdown: React.FC<Props> = (props) => {
+  const {} = props;
+
+  return <div>Dropdown</div>;
+};
+
+export default Dropdown;
