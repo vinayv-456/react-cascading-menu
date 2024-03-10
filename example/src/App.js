107f6249fb3b50649291e67a1b1b3bb1c6f939d5
vinayv-456 <vinayvallabineni456@gmail.com>
2024-03-09 20:06:01 -0500

    updated example

--- example/src/App.js
+++ example/src/App.js
@@ -3,10 +3,18 @@ import Index from "../../src/index.tsx"; // dynamic changes
 // import Index from "../../dist/bundle";
 
 const App = () => {
+  const options = [
+    { label: "Hyderabad-label", value: "Hyderabad-value" },
+    { label: "Vizag", value: "Vizag" },
+    { label: "Chennai", value: "Chennai" },
+    { label: "Benagaluru", value: "Benagaluru" },
+    { label: "Mumbai", value: "Mumbai" },
+    { label: "Delhi", value: "Delhi" },
+    { label: "Pune-label", value: "Pune-value" },
+  ];
   return (
     <div>
-      <h1>Your React App</h1>
-      <Index />
+      <Index options={options} displayValue="value" />
     </div>
   );
 };
