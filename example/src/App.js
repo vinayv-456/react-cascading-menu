ee1de8826335205a345f6db281a2d7fb6c71ec64
vinayv-456 <vinayvallabineni456@gmail.com>
2024-03-10 22:20:44 -0400

    refactor: update example

--- example/src/App.js
+++ example/src/App.js
@@ -1,20 +1,12 @@
 import React from "react";
 import Index from "../../src/index.tsx"; // dynamic changes
+import { menuGroup } from "./constants.js";
 // import Index from "../../dist/bundle";
 
 const App = () => {
-  const options = [
-    { label: "Hyderabad-label", value: "Hyderabad-value" },
-    { label: "Vizag", value: "Vizag" },
-    { label: "Chennai", value: "Chennai" },
-    { label: "Benagaluru", value: "Benagaluru" },
-    { label: "Mumbai", value: "Mumbai" },
-    { label: "Delhi", value: "Delhi" },
-    { label: "Pune-label", value: "Pune-value" },
-  ];
   return (
     <div>
-      <Index options={options} displayValue="value" />
+      <Index menuGroup={menuGroup} displayValue="value" />
     </div>
   );
 };
