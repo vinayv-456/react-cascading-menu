2f23b5f5925af3b854da113c104f33ef6b004101
vinayv-456 <vinayvallabineni456@gmail.com>
2024-06-15 13:17:59 -0400

    fix: remove the redundent results

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -66,17 +66,28 @@ function Search(props: Props) {
     const higherLevResults = allItems.filter((e) =>
       e.label.toLowerCase().includes(searchValue)
     );
+    let recentResults: number[][] = [];
+    let currentResults: number[][] = [];
     // TODO: need to check the results again
     higherLevResults.forEach((res) => {
       const { label, indexes } = res;
       const labelArr = label.split("=>");
+      recentResults = currentResults.length ? currentResults : recentResults;
+      currentResults = [];
       labelArr.forEach((e, index) => {
         const arg1 = e.trim().toLowerCase();
         const arg2 = searchValue;
         if (arg1.includes(arg2)) {
           const indexesPath = indexes.slice(0, index + 1);
+          currentResults.push(indexesPath);
           const labelsPath = labelArr;
-
+          // just need to check in the most recent results to remove the redundency
+          const isPrevAdded = recentResults.some((e) => {
+            return e.join("-").includes(indexesPath.join("-"));
+          });
+          if (isPrevAdded) {
+            return;
+          }
           matchedResults.push({
             label: e,
             labelsPath,
@@ -100,12 +111,15 @@ function Search(props: Props) {
         value={searchInp}
         onChange={(e) => setSearchInp(e.target.value)}
       />
-      {searchResults.map((e) => (
-        <SearchItem onClick={() => formSelection(e)}>
-          <div>{e.label}</div>
-          <div>{e.labelsPath.join(" => ")}</div>
-        </SearchItem>
-      ))}
+      {searchResults.map((e) => {
+        const { label, labelsPath, indexesPath } = e;
+        return (
+          <SearchItem onClick={() => formSelection(e)}>
+            <div>{label}</div>
+            <div>{labelsPath.slice(0, indexesPath.length).join(" => ")}</div>
+          </SearchItem>
+        );
+      })}
     </div>
   );
 }
